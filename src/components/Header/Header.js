f55537e818bae5cb5a9943a3ce5c2267724be85c
tantrung0204 <trungntce200064fpt@gmail.com>
2026-01-21 15:01:58 +0700

    navigation with Link + netsted route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">NewVerse</Navbar.Brand>
+                {/* <Navbar.Brand href="/">NewVerse</Navbar.Brand> */}
+                <Link to={"/"} className="navbar-brand">NewVerse</Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#users">Users</Nav.Link>
-                        <Nav.Link href="#admin">Admin</Nav.Link>
+                        <Link to={"/"} className="nav-link">Home</Link>
+                        <Link to={"/users"} className="nav-link">Users</Link>
+                        <Link to={"/admins"} className="nav-link">Admin</Link>
+                        {/* <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/users">Users</Nav.Link>
+                        <Nav.Link href="/admins">Admin</Nav.Link> */}
                     </Nav>
 
                     <Nav>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Admin from './components/Admin/Admin';
+import User from './components/User/User';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="users" element={<User />} />
+          <Route path="admins" element={<Admin />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
